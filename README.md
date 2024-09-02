0b878d1f4cd890757df06ebe0c8d411ffbd67e8c
庆宇 王 <1908630172@qq.com>
2024-09-02 15:50:50 +0800

    quickStart模块创建

---
+++ README.md
@@ -0,0 +1,3 @@
+## 这是许莫淇老师的 springboot 的多模块课程学习项目
+## 模块说明
+- spring-boot-quickstart: 快速起步模块
---
+++ spring-boot-quickstart/src/main/java/top/langxecho/springboot/quickstart/Application.java
@@ -0,0 +1,16 @@
+package top.langxecho.springboot.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 19086
+ * @version 1.0
+ * Create by 2024/9/2 15:46
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
